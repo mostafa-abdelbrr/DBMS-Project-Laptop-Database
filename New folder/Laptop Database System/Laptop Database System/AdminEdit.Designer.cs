63c4fa776ffe495e5f8b489c11f55fbaa0b479c9
Andrew Amir <andrew.ramses10@gmail.com>
2021-01-08 23:27:40 +0200

    small change

--- New folder/Laptop Database System/Laptop Database System/AdminEdit.Designer.cs
+++ New folder/Laptop Database System/Laptop Database System/AdminEdit.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.EditLTdg = new System.Windows.Forms.DataGridView();
             this.ConfirmEdit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.EditLTdg)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,11 +55,31 @@
             this.ConfirmEdit.UseVisualStyleBackColor = true;
             this.ConfirmEdit.Click += new System.EventHandler(this.ConfirmEdit_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Press (DEL) to delete";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Change Values then click confirm edit";
+            // 
             // AdminEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ConfirmEdit);
             this.Controls.Add(this.EditLTdg);
             this.Name = "AdminEdit";
@@ -65,6 +87,7 @@
             this.Load += new System.EventHandler(this.AdminEdit_Load);
             ((System.ComponentModel.ISupportInitialize)(this.EditLTdg)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +95,7 @@
 
         private System.Windows.Forms.DataGridView EditLTdg;
         private System.Windows.Forms.Button ConfirmEdit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
